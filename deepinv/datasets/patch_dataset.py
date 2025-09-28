1421965ee27b038528412650ef2a2f5dd0e9fe6f
nucli-vicky <vic.deridder@nuclivision.com>
2025-09-28 21:29:36 +0200

    3d DnCNN

--- deepinv/datasets/patch_dataset.py
+++ deepinv/datasets/patch_dataset.py
@@ -103,5 +103,5 @@ class AlternativePatchDataset3D(ImageDataset):
         fpath = os.path.join(self.im_dir, self.imgs[idx])
         shape = self.shapes[idx]
 
-
+        # We use random here: need to make a fix for deterministic behaviour based on seed --> seed worker function, see torch reproducibility page
         return load_np(fpath, start_coords=[random.randint(self.patch_size, shape[0] - self.patch_size), random.randint(self.patch_size, shape[1] - self.patch_size), random.randint(self.patch_size, shape[2] - self.patch_size)], patch_size=self.patch_size)
--- deepinv/models/dncnn.py
+++ deepinv/models/dncnn.py
@@ -39,21 +39,24 @@ class DnCNN(Denoiser):
         nf=64,
         pretrained="download",
         device="cpu",
+        dim=2
     ):
         super(DnCNN, self).__init__()
 
+        conv = {'2' :  nn.Conv2d, '3' : nn.Conv3d}[str(dim)]
+
         self.depth = depth
 
-        self.in_conv = nn.Conv2d(
+        self.in_conv = conv(
             in_channels, nf, kernel_size=3, stride=1, padding=1, bias=bias
         )
         self.conv_list = nn.ModuleList(
             [
-                nn.Conv2d(nf, nf, kernel_size=3, stride=1, padding=1, bias=bias)
+                conv(nf, nf, kernel_size=3, stride=1, padding=1, bias=bias)
                 for _ in range(self.depth - 2)
             ]
         )
-        self.out_conv = nn.Conv2d(
+        self.out_conv = conv(
             nf, out_channels, kernel_size=3, stride=1, padding=1, bias=bias
         )
 
